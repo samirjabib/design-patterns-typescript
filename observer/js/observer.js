268b9d8098febbea35fe84c41b0de08ebc47e7f3
samirjabib <samirdev9669@gmail.com>
2024-06-19 11:05:24 -0500

    study observer

--- observer/js/observer.js
+++ observer/js/observer.js
@@ -29,10 +29,15 @@ class Observer {
 const s = new Subject()
 
 const o1 = new Observer(data => console.log('hola soy el observador uno', data))
+const o2 = new Observer(data => div1.innerHTML = data)
+const o3 = new Observer(data => div2.innerHTML = data.split("").reverse().join(""))
 
 
 s.subscribe(o1)
+s.subscribe(o2)
+s.subscribe(o3)
 
 function change() {
     s.notify(myText.value)
 }
+
